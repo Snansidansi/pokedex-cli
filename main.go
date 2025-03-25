cfbb80e37b53b996e7eecf68897f126e3e4c5dfd
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-03-25 09:53:56 +0100

    Added cleanInput func with unit test

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+}
+
+func cleanInput(text string) []string {
+	text = strings.ToLower(text)
+	words := strings.Fields(text)
+	return words
 }
---
+++ repl_test.go
@@ -0,0 +1,49 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "hello world",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Hello WORlD",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Hello  World  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "      heLlo  world ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("The length of the actual slice (%d) does not match the length of the expected slice (%d)", len(actual), len(c.expected))
+			t.Errorf("Actual slice: %v", actual)
+			t.Errorf("Expected slice: %v", c.expected)
+			return
+		}
+
+		for i, actualWord := range actual {
+			expectedWord := c.expected[i]
+			if expectedWord != actualWord {
+				t.Errorf("The expected and actual are not the same: %s != %s", expectedWord, actualWord)
+				return
+			}
+		}
+	}
+}
