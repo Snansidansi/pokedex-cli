5e5a621c94c315a44737a966b9a1c63ff9b98134
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-03-28 16:58:12 +0100

    Removed unused data from the Pokemon struct

--- internal/pokeapi/pokemon.go
+++ internal/pokeapi/pokemon.go
@@ -20,44 +20,6 @@ type Pokemon struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
 	} `json:"forms"`
-	GameIndices []struct {
-		GameIndex int `json:"game_index"`
-		Version   struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"version"`
-	} `json:"game_indices"`
-	HeldItems []struct {
-		Item struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"item"`
-		VersionDetails []struct {
-			Rarity  int `json:"rarity"`
-			Version struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version"`
-		} `json:"version_details"`
-	} `json:"held_items"`
-	LocationAreaEncounters string `json:"location_area_encounters"`
-	Moves                  []struct {
-		Move struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"move"`
-		VersionGroupDetails []struct {
-			LevelLearnedAt int `json:"level_learned_at"`
-			VersionGroup   struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"version_group"`
-			MoveLearnMethod struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"move_learn_method"`
-		} `json:"version_group_details"`
-	} `json:"moves"`
 	Species struct {
 		Name string `json:"name"`
 		URL  string `json:"url"`
@@ -77,17 +39,4 @@ type Pokemon struct {
 			URL  string `json:"url"`
 		} `json:"type"`
 	} `json:"types"`
-	PastTypes []struct {
-		Generation struct {
-			Name string `json:"name"`
-			URL  string `json:"url"`
-		} `json:"generation"`
-		Types []struct {
-			Slot int `json:"slot"`
-			Type struct {
-				Name string `json:"name"`
-				URL  string `json:"url"`
-			} `json:"type"`
-		} `json:"types"`
-	} `json:"past_types"`
 }
