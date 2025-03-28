8a8d0f3a60937c0f119fcc3c6ab38dba0dd0af2a
Snansidansi <165903080+Snansidansi@users.noreply.github.com>
2025-03-28 13:14:10 +0100

    Added spacer in help printout

--- internal/commands/command_help.go
+++ internal/commands/command_help.go
@@ -7,11 +7,14 @@ import (
 )
 
 func commandHelp(_ *pokeapi.Config, _ ...string) error {
+	fmt.Println("")
+	fmt.Println("---------------------------------------------------")
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Print("Usage:\n\n")
 
 	for _, command := range GetCommands() {
 		fmt.Printf("%s: %s\n", command.Name, command.Description)
 	}
+	fmt.Println("---------------------------------------------------")
 	return nil
 }
